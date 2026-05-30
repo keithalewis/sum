198c3265697d13ef535d0b716f5c2503a61e6769
Keith A. Lewis <kal@kalx.net>
2026-05-29 21:05:35 -0400

    dot

--- sum.scala
+++ sum.scala
@@ -1,15 +1,29 @@
-
-// 0, 1, 2, ...
-def Time() = Iterator.from(0)
-
-/*
 def choose(n: Int, k: Int): BigInt = {
   require(n >= 0 && k >= 0 && k <= n, "need 0 <= k <= n")
-  val kk = math.min(k, n - k) // use symmetry to reduce work
-  (1 to kk).foldLeft(BigInt(1)) { (acc, i) =>
-    acc * (n - kk + i) / i
+  val j = math.min(k, n - k) // use symmetry to reduce work
+  (1 to j).foldLeft(BigInt(1)) { (acc, i) =>
+    acc * (n - j + i) / i
   }
 }
+
+// 0, 1, 2, ...
+//def Time() = Iterator.from(0)
+
+def dot[A: Numeric, B: Numeric](xs: Seq[A], ys: Seq[B]): Double =
+  require(xs.length == ys.length, "dot: sequences must have the same length")
+  val na = summon[Numeric[A]]
+  val nb = summon[Numeric[B]]
+  xs.iterator
+    .zip(ys.iterator)
+    .map((x, y) => na.toDouble(x) * nb.toDouble(y))
+    .sum
+
+@main def run(): Unit =
+  println(choose(50, 30))
+  println(dot(Seq(1, 2, 3), Seq(4.0, 5.0, 6.0)))
+
+/*
+
 
 def partitionBy[A, K](s: Set[A])(f: A => K): Set[Set[A]] =
   s.groupBy(f).values.map(_.toSet).toSet
